37f7498681ccac64d34137464e56f0d1926019b5
alimaktabi <maktabi876@gmail.com>
2022-07-22 12:08:49 +0430

    moved github token from .env to .env.local for security reasons
    added nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import type { AppProps } from "next/app"
 import { FC } from "react"
+import nProgress from "nprogress"
+import { Router } from "next/router"
 
 import "../styles/globals.css"
+import "nprogress/nprogress.css"
+
+Router.events.on("routeChangeStart", nProgress.start)
+
+Router.events.on("routeChangeError", nProgress.done)
+
+Router.events.on("routeChangeComplete", nProgress.done)
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
